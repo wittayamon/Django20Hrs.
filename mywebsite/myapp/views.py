64147043b78708428a625868e3e5b9ddb76d9b01
Witty Endurence <81568480+wittayamon@users.noreply.github.com>
2023-04-23 11:46:21 +0700

    เปลี่ยน httpReponse เป้น render

--- mywebsite/myapp/views.py
+++ mywebsite/myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def Homepage(request):
-    return HttpResponse('<h1>Hello Word</h1>')
+    return render(request, 'myapp/homepage.html')
 
